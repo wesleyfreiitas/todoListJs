b2cc81ffec281b9e010fe616e8b77de02954cedc
Wesley Freitas <wesley8bu@gmail.com>
2022-07-23 02:12:27 -0300

    Gerando li dinamicamente

--- js/todolist.js
+++ js/todolist.js
@@ -2,19 +2,37 @@
     const itemInput = document.getElementById("item-input")
     const todoAddForm = document.getElementById("todo-add")
     const ul = document.getElementById("todo-list")
+    const lis = ul.getElementsByTagName("li")
+
+    
+
+    function addEventLi(li){
+        li.addEventListener("click", function(){
+            console.log(this)
+        })
+    }
+
+    function addTask(task){
+        const li = document.createElement("li")
+        li.className = "todo-item"
+        const p = document.createElement("p")
+        p.className = "task-name"
+        p.textContent = task
+        li.appendChild(p)
+        ul.appendChild(li)
+        addEventLi(li)
+    }
 
     todoAddForm.addEventListener("submit", function(e){
         e.preventDefault
-        ul.innerHTML += `
-            <li class="todo-item">
-                <p class="task-name">${itemInput.value}</p>
-            </li>
-        `
+        addTask(itemInput.value)
         itemInput.value = ""
         itemInput.focus()
+    });
+
+    [...lis].forEach(li => {
+        addEventLi(li)
     })
-
-
 
 
 })()
